ec8ca7df4142d32fa0736eb7dd651fe5bbfa7ba5
Mohammad Yasir <mdcode2021@gmail.com>
2022-05-02 17:59:48 +0530

    Small Correction

--- Sources/Upstream/Public/Upstream.swift
+++ Sources/Upstream/Public/Upstream.swift
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import UIKit
+import SwiftUI
 
 public struct UpstreamButton: View {
     @ObservedObject private var upstream: Upstream
